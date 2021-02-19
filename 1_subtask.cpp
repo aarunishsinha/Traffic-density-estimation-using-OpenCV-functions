b861d8ac8cf59c409846bad2a9c94d286be6e123
aarunishsinha <aarunish.sinha@gmail.com>
2021-02-19 18:10:54 +0530

    Source Files uploaded

---
+++ 1_subtask.cpp
@@ -0,0 +1,14 @@
+#include <opencv2/opencv.hpp>
+
+using namespace cv;
+using namespace std;
+
+
+int main( int argc, char** argv)
+{
+
+    // Read in the image.
+    Mat im_src = imread("empty.jpg");
+
+    return 0;
+}
---
+++ Makefile
@@ -0,0 +1,10 @@
+all: compile run
+
+compile:
+		g++ 1_subtask.cpp -o 1_subtask -pthread -std=c++11 `pkg-config --cflags --libs opencv4`
+
+run:
+		./1_subtask
+
+clean:
+		rm 1_subtask

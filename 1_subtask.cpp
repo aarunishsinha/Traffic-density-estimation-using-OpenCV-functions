4de3a45dfdf49a7d5fc55d0c0c1102d7c3da40ff
aarunishsinha <aarunish.sinha@gmail.com>
2021-02-19 21:44:22 +0530

    Added Grayscaling

--- 1_subtask.cpp
+++ 1_subtask.cpp
@@ -1,4 +1,5 @@
 #include <opencv2/opencv.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
 
 using namespace cv;
 using namespace std;
@@ -9,6 +10,15 @@ int main( int argc, char** argv)
 
     // Read in the image.
     Mat im_src = imread("empty.jpg");
+
+    // Converting RGB image to grayscale
+    Mat grey_img;
+    cvtColor(im_src, grey_img, COLOR_BGR2GRAY);
+
+    namedWindow("Image", 1);
+
+    imshow("Image", grey_img);
+    waitKey(0);
 
     return 0;
 }

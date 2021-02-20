79032870e7a624fbf912b526432e01964145daf4
aarunishsinha <aarunish.sinha@gmail.com>
2021-02-20 21:55:03 +0530

    Add mouseClick and Transformation

--- 1_subtask.cpp
+++ 1_subtask.cpp
@@ -4,6 +4,18 @@
 using namespace cv;
 using namespace std;
 
+struct data{
+    vector<Point2f> points;
+    Mat img;
+};
+
+void mouseClick(int event, int x, int y, int flags, void* userdata){
+    if (event==EVENT_LBUTTONDOWN){
+        data *image = ((data *) userdata);
+        imshow("Image", image->img);
+        image->points.push_back(Point2f(x,y));
+    }
+}
 
 int main( int argc, char** argv)
 {
@@ -15,10 +27,39 @@ int main( int argc, char** argv)
     Mat grey_img;
     cvtColor(im_src, grey_img, COLOR_BGR2GRAY);
 
+    MatSize s = grey_img.size;
+    Size size(s[0], s[1]);
+    // Mat im_projected = Mat::zeros(s, CV_8UC4);
+    Mat im_projected (size, CV_8UC4);
+    im_projected = 0;
+
+    vector<Point2f> pts_projected;
+
+    pts_projected.push_back(Point2f(0,0));
+    pts_projected.push_back(Point2f(size.width-1,0));
+    pts_projected.push_back(Point2f(size.width-1,size.height-1));
+    pts_projected.push_back(Point2f(0,size.height-1));
+
     namedWindow("Image", 1);
 
-    imshow("Image", grey_img);
+    // Mouse Input
+    Mat temp = grey_img.clone();
+
+    data take_input;
+    take_input.img = temp;
+
+    setMouseCallback("Image", mouseClick, &take_input);
+
+    imshow("Image", temp);
     waitKey(0);
+
+    // Projecting image
+    Mat transform = findHomography(take_input.points, pts_projected);
+    warpPerspective(grey_img, im_projected, transform, size);
+
+    imshow("Image", im_projected);
+    waitKey(0);
+
 
     return 0;
 }

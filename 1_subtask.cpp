e22879e498d3905f59cd52b02d5b2dee679060e7
mohit2023 <mohit7153@gmail.com>
2021-02-21 21:15:15 +0530

    Resized image window if image resolution greater than screen resolution

--- 1_subtask.cpp
+++ 1_subtask.cpp
@@ -1,6 +1,7 @@
 #include <opencv2/opencv.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/highgui.hpp>
+#include <X11/Xlib.h>
 
 using namespace cv;
 using namespace std;
@@ -10,6 +11,8 @@ struct data{
     Mat img;
 };
 
+int SCREEN_WIDTH,SCREEN_HEIGHT;
+
 void mouseClick(int event, int x, int y, int flags, void* userdata){
     if (event==EVENT_LBUTTONDOWN){
         data *image = ((data *) userdata);
@@ -18,11 +21,40 @@ void mouseClick(int event, int x, int y, int flags, void* userdata){
     }
 }
 
+void getScreenResolution(){
+	Display* disp = XOpenDisplay(NULL);
+	Screen* screen = DefaultScreenOfDisplay(disp);
+	SCREEN_WIDTH = screen->width;
+	SCREEN_HEIGHT = screen->height;
+}
+
+ 
+void createWindow(string WindowName, Mat &img){
+	// To resize image window if image resolution is more than screen resolution.
+	
+	namedWindow(WindowName, WINDOW_KEEPRATIO);
+	
+	if(img.size[1]>SCREEN_WIDTH || img.size[0]>SCREEN_HEIGHT){
+		double scale_width= (double)(SCREEN_WIDTH)/img.size[1];
+		double scale_height= (double)(SCREEN_HEIGHT)/img.size[0];
+		double scale=min(scale_width,scale_height);
+		
+		int window_width=scale*img.size[1];
+		int window_height=scale*img.size[0];
+		
+		resizeWindow(WindowName,window_width,window_height);
+	}
+}
+
 int main( int argc, char** argv)
-{
+{	
+	// Initialize SCREEN_WIDTH and SCREEN_HEIGHT
+    getScreenResolution();
+    
 
     // Read in the image.
     Mat im_src = imread("empty.jpg");
+    
 
     // Converting RGB image to grayscale
     Mat grey_img;
@@ -47,12 +79,13 @@ int main( int argc, char** argv)
     pts_projected.push_back(Point2f(800,52));
     pts_projected.push_back(Point2f(800,830));
     pts_projected.push_back(Point2f(472,830));
-
-    namedWindow("Image", 1);
-
+    
+	
     // Mouse Input
     Mat temp = grey_img.clone();
-
+	//namedWindow("Image", 1);
+	createWindow("Image",temp);
+	
     data take_input;
     take_input.img = temp;
 
@@ -61,21 +94,25 @@ int main( int argc, char** argv)
     imshow("Image", temp);
     waitKey(0);
 
+	
     // Projecting image
     Mat transform = findHomography(take_input.points, pts_projected);
     warpPerspective(grey_img, im_projected, transform, size);
     
-    imshow("Image", im_projected);
+    createWindow("Projected Image", im_projected);
+    imshow("Projected Image", im_projected);
     waitKey(0);
+    
     
     // Cropping image
     Rect croppedRectangle = Rect(472,52,328,778);
     Mat croppedImage = im_projected(croppedRectangle);
     
-    namedWindow("Cropped",WINDOW_FREERATIO);
+    createWindow("Cropped",croppedImage);
     imshow("Cropped",croppedImage);
     waitKey(0);
-
+	
+	destroyAllWindows();
 
     return 0;
 }
--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 all: compile run
 
 compile:
-		g++ 1_subtask.cpp -o 1_subtask -pthread -std=c++11 `pkg-config --cflags --libs opencv4`
+		g++ 1_subtask.cpp -o 1_subtask -lX11 -pthread -std=c++11 `pkg-config --cflags --libs opencv4`
 
 run:
 		./1_subtask
